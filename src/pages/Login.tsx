75a389fbc079761c0b0ef329da53f6d06e988607
LofizDev <khoiilam.dev@gmail.com>
2023-06-18 17:35:29 +0700

    fix build

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import background from '../assets/background.png';
-;
+
 
 const Login: React.FC = () => {
     const navigate = useNavigate();
